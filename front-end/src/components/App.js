83fa2f8b6b2b2fdf893877f0e9d2bae72e39284a
Mateus Figueiredo Pereira <figueiredo1497@gmail.com>
2022-11-04 18:37:43 -0300

    add simple style

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -1,25 +1,28 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-
+import axios from "axios";
+import { useEffect, useState } from "react";
+import '../styles/Global.css';
 function App() {
   const [student, setStudent] = useState(null);
 
-  useEffect(async () => {
-    const API_URL = process.env.REACT_APP_BASE_URL;
-    try {
-      const response = await axios.get(`${API_URL}/students/random`);
-      const student = response.data;
-      if (!student) {
-        alert('Xablau! Não há estudantes cadastrados para o sorteio!');
-      } else {
-        setStudent(student);
+  useEffect(() => {
+    async function fetchData() {
+      const API_URL = process.env.REACT_APP_BASE_URL;
+      try {
+        const response = await axios.get(`${API_URL}/students/random`);
+        const student = response.data;
+        if (!student) {
+          alert("Xablau! Não há estudantes cadastrados para o sorteio!");
+        } else {
+          setStudent(student);
+        }
+      } catch (error) {
+        alert("Não foi possível realizar o sorteio!");
       }
-    } catch (error) {
-      alert('Não foi possível realizar o sorteio!');
     }
+    fetchData();
   }, []);
 
-  return student ? <h1>{student.name}</h1> : 'Carregando...';
+  return student ? <h1>{student.name}</h1> : "Carregando...";
 }
 
 export default App;
